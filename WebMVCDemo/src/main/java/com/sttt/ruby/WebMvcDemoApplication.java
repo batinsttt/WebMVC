a5454580099b6df6425c4d065c3b852246052e14
tintb <batinsttt@gmail.com>
2019-01-17 18:25:55 +0700

    Tin commit demo

---
+++ WebMVCDemo/src/main/java/com/sttt/ruby/WebMvcDemoApplication.java
@@ -0,0 +1,14 @@
+package com.sttt.ruby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMvcDemoApplication.class, args);
+	}
+
+}
+
